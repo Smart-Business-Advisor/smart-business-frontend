09766204ee000ab68f1ba2cd7d35ac0ff54ad4d6
omniyanasser <https://github.com/omniyanasser>
2025-11-25 01:36:59 +0200

    Add login form validation + update Tailwind styles

--- src/pages/auth/auth-schema/login-schema.js
+++
@@ -1,6 +0,0 @@
-import { z } from "zod";
-
-export const formSchema = z.object({
-  Email: z.string().email("Please enter a valid email address"),
-  Password: z.string().min(6, "Password must be at least 6 characters"),
-});
